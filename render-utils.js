1d1d5d7125e8f0b97a822c44dd564509d1b5b534
SusanBird <susanbird391@gmail.com>
2022-03-22 16:33:50 -0700

    DOM for renderAthlete

---
+++ render-utils.js
@@ -0,0 +1,13 @@
+export function renderAthlete() {
+
+    const athleteEl = document.createElement('div');
+    const nameEl = document.createElement('h2');
+    const careerHighlightsEl = document.createElement('p');
+    const yearsWithBTCEl = document.createElement('p');
+    const nationalRecordsEl = document.createElement('p');
+    const ageEl = document.createElement('p');
+    const personalBestsEl = document.createElement('p');
+
+    athleteEl.classList.add('BTC-athlete');
+
+}
