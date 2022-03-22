85836a5ac35253925ce837c5afd9dd423de7dc8a
SusanBird <susanbird391@gmail.com>
2022-03-22 16:47:39 -0700

    add load event listner for list page

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
-
+import { getAllAthletes } from "./fetch-utils";
+import { renderAthlete } from "./render-utils";
 // let state
+
+const athletesListEL = document.querySelector('.athletes-list');
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+window.addEventListener('load', async () => {
+    const athletes = await getAllAthletes();
+
+    for (let athlete of athletes) {
+
+        const athleteEl = renderAthlete(athlete);
+
+        athletesListEL.append(athleteEl);
+    }
+
+});
