b6336459c8d2be39a8b7a0e5b42afb7592547d7c
SusanBird <susanbird391@gmail.com>
2022-03-22 17:51:04 -0700

    fixed supabase issue

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 // import functions and grab DOM elements
-import { getAllAthletes } from "./fetch-utils";
-import { renderAthlete } from "./render-utils";
+import { getAllAthletes } from './fetch-utils.js';
+import { renderAthlete } from './render-utils.js';
 // let state
 
 const athletesListEL = document.querySelector('.athletes-list');
-
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 
 window.addEventListener('load', async () => {
-    const athletes = await getAllAthletes();
 
+    const athletes = await getAllAthletes();
     for (let athlete of athletes) {
 
         const athleteEl = renderAthlete(athlete);
@@ -20,4 +19,6 @@ window.addEventListener('load', async () => {
         athletesListEL.append(athleteEl);
     }
 
+   
 });
+
